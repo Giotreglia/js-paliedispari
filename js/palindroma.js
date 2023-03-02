d0ef17107bbeb5af471a7b0f5684be613f7d862d
Giotreglia <giovanni.treglia@gmail.com>
2023-03-02 15:44:57 +0100

    Programma per verifica se parola palindroma funzionante (la funzione di comparazione di array "arrayEquals" l'ho presa da internet)

---
+++ js/palindroma.js
@@ -0,0 +1,54 @@
+// Parola palindroma
+
+/*
+    Chiedere all'utente di inserire una parola
+    Stabilire se la parola inserita è palindroma
+*/
+
+// Chiedo all'utente di inserire una parola
+let parolaUtente = prompt("Inserisci una parola");
+
+// Definisco se parola è palindroma
+
+console.log(parolaPalindroma(parolaUtente)); 
+
+//FUNZIONI
+
+// Funzione per verifica se una parola è palindroma
+
+function parolaPalindroma(parola) {
+
+    let parolaDritta = [];
+    let parolaReverse = [];
+    let risultato;
+
+    for (let i = 0; i < parolaUtente.length; i++) {
+        parolaDritta.push(parolaUtente[i]);
+    }
+
+    console.log(parolaDritta);
+
+    for (let i = parolaUtente.length - 1; i >= 0; i--) {
+        parolaReverse.push(parolaUtente[i]); 
+    }
+
+    console.log(parolaReverse);
+
+    if (arrayEquals(parolaDritta, parolaReverse)) {
+        risultato = "La parola è palindroma";
+    } else {
+        risultato = "La parola non è palindroma";
+    }
+        
+    return risultato;
+} 
+
+// Funzione per verificare se due array sono uguali
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
